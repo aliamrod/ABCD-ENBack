0fd8ab958e712446521aceb2d62a83eb610a458a
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2024-08-29 13:18:27 -0500

    Create redo-masterlist.py

---
+++ process-file-pipeline/redo-master list.py
@@ -0,0 +1,13 @@
+# mri_y_tfmr_nback_beh.csv
+
+#mri_y_qc_raw_tfmr_nback.csv
+
+# mri_y_qc_motion.csv
+
+# mri_y_qc_man_post_t2w.csv
+
+# mri_y_qc_man_post_fmr.csv
+#mri_y_qc_incl.csv
+# mri_y_qc_auto_post.csv
+
+#mri_y_adm_info.csv
